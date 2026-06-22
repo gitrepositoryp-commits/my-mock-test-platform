89bd318aecdc14032271f957c36fa446ee138871
rrb-edu <koduru367@gmail.com>
2026-06-22 08:14:29 +0530

    Fix protect middleware import in payment route

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -4,9 +4,8 @@ const Razorpay = require("razorpay");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const User = require("../models/User");
-
 const router = express.Router();
-
+const protect = require("../middleware/authMiddleware");
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
